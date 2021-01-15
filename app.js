74200249e42bacf0378a6c1e1bdaaf3a617adc7d
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-01-15 14:14:41 -0600

    added 3 variables: developer's name, number of US states, and a variable that adds 2 numbers

---
+++ app.js
@@ -0,0 +1,8 @@
+// this is a variable that has a value of the developer's name
+var name = "Gabriel";
+
+// this is a constant variable the lists the # of US states
+const number_us_states = 50;
+
+// this is a variable the adds two numbers together
+var add_num = 5 + 4;
